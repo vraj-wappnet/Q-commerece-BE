23379fc19afd77f043649050751248cbaf68210a
vraj-wappnet <vraj.p@wappnet.com>
2026-03-24 10:41:22 +0530

    Resolve order module error

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -7,12 +7,13 @@ import { Cart } from "src/cart/entity/cart.entity";
 import { CartItem } from "src/cart/entity/cart-item.entity";
 import { Product } from "src/products/entity/product.entity";
 import { DeliveryProfile } from "src/delivery_profiles/entity/delivery-profile.entity";
+import { DeliveryAssignment } from "src/order_delivery_assignment/entity/delivery_assignment.entity";
 import { orderController } from "./orders.controller";
 import { NotificationModule } from "../notifications/notification.module";
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Order, OrderItem, Cart, CartItem, Product, DeliveryProfile]),
+    TypeOrmModule.forFeature([Order, OrderItem, Cart, CartItem, Product, DeliveryProfile, DeliveryAssignment]),
     NotificationModule
   ],
   providers: [OrderService],
